37c129ae4e56cf3d519f4095b0394bb56d1d8ed0
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-21 13:40:19 +0530

    signup page created

---
+++ lib/views/authentication/signup/signup.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(size.width / 16),
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('SIGNUP'),
+              SizedBox(
+                height: size.height / 10,
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  hintText: 'Enter email or mob no',
+                ),
+                // validator: (value) => validateEmail(value!),
+                // onTap: () {
+                //   setState(() {
+                //     emailTapped = true;
+                //   });
+                // },
+              ),
+              SizedBox(
+                height: size.height / 18,
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  hintText: 'Enter password',
+                ),
+                // validator: (value) => validateEmail(value!),
+                // onTap: () {
+                //   setState(() {
+                //     emailTapped = true;
+                //   });
+                // },
+              ),
+              SizedBox(
+                height: size.height / 18,
+              ),
+              SizedBox(
+                width: size.width,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height / 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Already have an account ? "),
+                  GestureDetector(
+                    // onTap: () => Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => const SigninScreen(),
+                    //   ),
+                    // ),
+                    child: const Text(
+                      "Signin",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
