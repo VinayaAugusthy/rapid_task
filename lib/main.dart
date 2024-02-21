3d5e6d381c132a29cf7d7a9f259e81030aae8616
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-21 13:44:28 +0530

    signin screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rapid_task/views/authentication/signup/signup.dart';
+import 'package:rapid_task/views/authentication/signin/signin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignupScreen(),
+      home: const SigninScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
