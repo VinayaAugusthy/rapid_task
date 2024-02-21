bc824ebcfcf1582c630dcb7dc1b4659e883e53a5
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-21 23:00:21 +0530

    usermodel changed

--- lib/models/authentication/user_model.dart
+++ lib/models/authentication/user_model.dart
@@ -1,5 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
-final String? id;
-final String? email;
-UserModel({ this.id, this.email,});
+  final String id;
+  final String email;
+  final String username;
+  final String photoUrl;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.photoUrl,
+  });
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "uid": id,
+        "photoUrl": photoUrl,
+        "username": username,
+      };
+
+  static UserModel fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return UserModel(
+      username: snapshot['username'],
+      email: snapshot['email'],
+      id: snapshot['id'],
+      photoUrl: snapshot['photoUrl'],
+    );
+  }
 }
