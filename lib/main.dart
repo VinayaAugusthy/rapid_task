5a0e2af94003796e7d914f0a7806d29bdca74ed6
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-21 16:37:20 +0530

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rapid_task/viewmodels/application/bloc/authentication_bloc.dart';
 import 'package:rapid_task/views/authentication/authentication.dart';
-import 'package:rapid_task/views/authentication/signin/signin.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
