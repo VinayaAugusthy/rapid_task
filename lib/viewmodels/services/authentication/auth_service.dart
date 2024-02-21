115136d5cba9ad2f0639121a466b9cb856d0f27c
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-21 15:39:15 +0530

    authservice created

---
+++ lib/viewmodels/services/authentication/auth_service.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: nullable_type_in_catch_clause, avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rapid_task/models/authentication/user_model.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+
+  Future<UserModel?> signUpUser(
+    String email,
+    String password,
+  ) async {
+    try {
+      final UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      final User? firebaseUser = userCredential.user;
+      if (firebaseUser != null) {
+        return UserModel(
+          id: firebaseUser.uid,
+          email: firebaseUser.email ?? '',
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e.toString());
+    }
+    return null;
+  } 
+
+   Future<void> signOutUser() async {
+      final User? firebaseUser = FirebaseAuth.instance.currentUser;
+    if (firebaseUser != null) {
+      await FirebaseAuth.instance.signOut();
+    }
+  }
+}
