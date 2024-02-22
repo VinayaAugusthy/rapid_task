a646104b6bdfac583dca5f9f9c6ad759487bd2a4
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-22 18:27:29 +0530

    formatting

--- lib/viewmodels/services/authentication/auth_service.dart
+++ lib/viewmodels/services/authentication/auth_service.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: nullable_type_in_catch_clause, avoid_print, use_build_context_synchronously
+// ignore_for_file: nullable_type_in_catch_clause, avoid_print, use_build_context_synchronously, unused_local_variable
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
